388a401739758c8aed7d5f34a12830a4691c3927
Jimmy Hough <jimmyhoughjr@me.com>
2022-12-12 22:27:28 -0600

    Initial Commit

---
+++ Boiler/BoilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilerApp.swift
+//  Boiler
+//
+//  Created by Jimmy Hough Jr on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BoilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
